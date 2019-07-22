3976deeabc4ef881c1bc14789b7bdc657565c6d4
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-07-22 16:15:21 -0500

    added email and password field

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,21 +1,31 @@
 import React, { Component} from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
+import { thisExpression } from '@babel/types';
 
 class LoginForm extends Component {
-    state = { text: '' };
+    state = { email: '' };
+    state = { password: '' };
     
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput
-                        value = { this.state.text }
-                        onChangeText = { text => this.setState( { text } )}
+                    <Input
+                        placeholder = "user@mail.com"
+                        label = "Email"
+                        value = { this.state.email }
+                        onChangeText = { email => this.setState( { email } )}
                         style = {{ height: 40, width: 100 }}
                     />
                 </CardSection>
-                <CardSection/>
+                <CardSection>
+                    <Input
+                        placeholder = "password"
+                        label = "Password"
+                        value = { this.state.value }
+                        onChangeText = { password => this.setState( { password })}
+                    />
+                </CardSection>
                 <CardSection>
                     <Button>
                         Log in

c452ec72f9932c2f8ed3e110f40c651c985eb2b9
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-07-22 15:48:48 -0500

    firebase set up, text input component

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Firebase from 'firebase';
+import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-    // Firebase initialization
-   /* componentWillMount() {
+    UNSAFE_componentWillMount() {
         firebase.initializeApp({
-            apiKey: 'AIzaSyBNIEkD90ruqjiV1uritedCOG06E3iL59w',
-            authDomain: 'auth-2850a.firebaseapp.com',
-            databaseURL: 'https://auth-2850a.firebaseio.com',
-            projectId: 'auth-2850a',
-            storageBucket: '',
-            messagingSenderId: '551445583799',
-            appId: '1:551445583799:web:b37df706f87c0330'
+            apiKey: "AIzaSyBNIEkD90ruqjiV1uritedCOG06E3iL59w",
+            authDomain: "auth-2850a.firebaseapp.com",
+            databaseURL: "https://auth-2850a.firebaseio.com",
+            projectId: "auth-2850a",
+            storageBucket: "",
+            messagingSenderId: "551445583799",
+            appId: "1:551445583799:web:b37df706f87c0330"
         });
-    }*/
+    }
     render() {
         return (
             <View>

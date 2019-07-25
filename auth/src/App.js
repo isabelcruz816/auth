348beee791ac1b6e0d25add4677f3c2dc4a8eae2
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-07-25 12:16:28 -0500

    renderContent helper method to show log out button

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
     state = { loggedIn: false };
 
     UNSAFE_componentWillMount() {
+        
         firebase.initializeApp({
             apiKey: "AIzaSyBNIEkD90ruqjiV1uritedCOG06E3iL59w",
             authDomain: "auth-2850a.firebaseapp.com",
@@ -18,7 +19,7 @@ class App extends Component {
             appId: "1:551445583799:web:b37df706f87c0330"
         });
 
-        firebase.auth.OAuthStateChanged((user) => {
+        firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 this.setState({ loggedIn: true });
             }
@@ -26,15 +27,27 @@ class App extends Component {
                 this.setState({ loggedIn: false });
             }
         });
-    }
+    };
+
+    renderContent() {
+        if (this.state.loggedIn) {
+            return(
+                <Button>
+                    Log Out
+                </Button>
+            );
+        }
+        return <LoginForm/>;
+    };
+
     render() {
         return (
             <View>
                 <Header headerText = "Authentication"></Header>
-                <LoginForm/>
+                { this.renderContent() }
             </View>
         );
-    }
+    };
 };
 
 export default App;
